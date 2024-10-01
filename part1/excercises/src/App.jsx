1e6150381c0a4808ece948df87b69e9ffd08d197
Joel Ismar Florentin <j.ismar00@gmail.com>
2024-09-30 20:18:53 -0400

    completed exercise 1.7

--- part1/excercises/src/App.jsx
+++ part1/excercises/src/App.jsx
@@ -5,23 +5,62 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positives, setPositives] = useState(0)
+  const [setscore, setSetscore] = useState(0)
+
+
+
+
+  const handleClick = (opt) => {
+    const updateAll = all + 1
+    let updateScore = setscore
+    if (opt === 'good') {
+      handleclickGood()
+      updateScore += 1
+      setSetscore(updateScore)
+
+    } else if (opt === 'neutral') {
+      handleclickNeutral()
+    } else if (opt === 'bad') {
+      updateScore -= 1
+      setSetscore(updateScore)
+      handleclickBad()
+    }
+    setAll(updateAll)
+    calculatePositive(updateAll)
+    calculateAverage(updateScore, updateAll)
+
+  }
 
   const handleclickGood = () => {
-    setGood(good + 1)
+    const update = good + 1
+    setGood(update)
   }
   const handleclickNeutral = () => {
-    setNeutral(neutral + 1)
+    const update = neutral + 1
+    setNeutral(update)
   }
   const handleclickBad = () => {
-    setBad(bad + 1)
+    const update = bad + 1
+    setBad(update)
   }
+
+  const calculateAverage = (score, all) => {
+    setAverage(score / all)
+  }
+  const calculatePositive = (updateAll) => {
+    setPositives(((good * 100) / updateAll))
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button type="button" onClick={handleclickGood}>good</button>
-        <button type="button" onClick={handleclickNeutral}>neutral</button>
-        <button type="button" onClick={handleclickBad}>bad</button>
+        <button type="button" onClick={() => handleClick('good')}>good</button>
+        <button type="button" onClick={() => handleClick('neutral')}>neutral</button>
+        <button type="button" onClick={() => handleClick('bad')}>bad</button>
 
       </div>
       <h2>Statistics</h2>
@@ -30,7 +69,9 @@ const App = () => {
           <li> <span>good</span> <span>{good}</span> </li>
           <li> <span>neutral</span> <span>{neutral}</span> </li>
           <li> <span>bad</span> <span>{bad}</span> </li>
-
+          <li> <span>all</span> <span>{all.toString()}</span> </li>
+          <li> <span>average</span> <span>{average.toString()}</span> </li>
+          <li> <span>positive</span> <span>{positives.toString()}</span> </li>
         </ul>
       </div>
     </div>
