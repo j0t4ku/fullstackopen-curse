95f92bc2291a24c8f16456a081384298fe59ce54
Joel Ismar Florentin <j.ismar00@gmail.com>
2024-10-18 19:06:50 -0300

    completed exercises 4.4

--- part4/bloglist/tests/list_test.test.js
+++ part4/bloglist/tests/list_test.test.js
@@ -1,10 +1,19 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const blogList = require('../utils/const')
 
 test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
+})
+
+describe("Total Likes", () => {
+
+    test("totalLikes", () => {
+        const result = listHelper.totalLikes(blogList)
+        assert.strictEqual(result, 36)
+    })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogList) => {
+    return blogList.length === 0
+        ? 0
+        : blogList.reduce((sum, post) => sum + post.likes, 0);
+};
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
